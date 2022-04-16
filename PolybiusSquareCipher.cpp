5e3f3236088924e6b9d418478859da2e8eb7754f
Yllka Nishevci <75842785+yllkann@users.noreply.github.com>
2022-04-17 09:01:54 +1100

    Update PolybiusSquareCipher.cpp

--- PolybiusSquareCipher.cpp
+++ PolybiusSquareCipher.cpp
@@ -1,1 +1,14 @@
-
+#include <iostream>
+#include <string>
+#include<algorithm>
+using namespace std;
+int main()
+{
+    string x, y;
+    cout << "Do you want to encrypt or decrypt ?" << endl;
+    cin >> y;
+    while (y != "encrypt" && y != "decrypt") {
+        cout << "Wrong choice  " << endl;
+        cout << "please enter again :  ";
+        cin >> y;
+    }
