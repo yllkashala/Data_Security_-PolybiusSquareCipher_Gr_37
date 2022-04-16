4ad3c18bda397a383a2c9675ade935ae3e0e86fb
yllkashala <93856740+yllkashala@users.noreply.github.com>
2022-04-17 00:03:18 +0200

    Update PolybiusSquareCipher.cpp

--- PolybiusSquareCipher.cpp
+++ PolybiusSquareCipher.cpp
@@ -12,3 +12,23 @@ int main()
         cout << "please enter again :  ";
         cin >> y;
     }
+char key;
+    //Take the key
+    for (int i = 0; i < 5; i++) {
+        cout << "Enter the key : ";
+        cin >> key;
+        //save keys in string    
+        x += key;
+    }
+    char table[6][6] = { { ' ' ,x[0],x[1],x[2],x[3] ,x[4]},
+                           { x[0],'A','B','C','D','E'},
+                           { x[1],'F','G','H','I','K'},
+                           { x[2],'L','M','N','O','P'},
+                           { x[3],'Q','R','S','T','U'},
+                           { x[4],'V','W','X','Y','Z'} };
+    for (int i = 0; i < 6; i++) {
+        for (int j = 0; j < 6; j++) {
+            cout << table[i][j] << " ";
+        }
+        cout << endl;
+    }
